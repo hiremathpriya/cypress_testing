c3743289667ec33592301467b0ed2f8578edae2d
Priya Hiremath <priyahrish18@gmail.com>
2018-08-19 20:45:00 +1200

    added some more bnz tests

--- cypress/integration/bnz/home.spec.js
+++ cypress/integration/bnz/home.spec.js
@@ -10,7 +10,23 @@ describe('bnz home page', () => {
 })
 describe('it will find the content on the page' , () => {
     it('finds the content on the page,"Register"', () => {
-        cy.visit('bnz.co.nz')
+        // cy.visit('bnz.co.nz')
         cy.contains('Register').click()
+    })
+})
+describe('it finds the login on the register page and clicks it', () => {
+    it('find a Go to Login and click', () => {
+        cy.contains('Go to Login').click()
+    })
+})
+describe('input access number and password and login', () => {
+    it('adds the access and password and click on the login', () => {
+        cy.get('#accessId').type('12345')
+        cy.get('#password').type('bomaby')
+        cy.get("#login").click()
+        cy.find("h3").contains('There were two errors on the page')
+        
+        
+        
     })
 })
